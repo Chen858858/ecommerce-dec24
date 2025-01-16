517625a26bf97298b8bed97a2d9cd084aa248ef6
Chen858858 <junchenwang5@gmail.com>
2025-01-16 11:57:54 -0800

    imported CategoryTag to SearchItem

--- src/components/SearchItem.jsx
+++ src/components/SearchItem.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { Card } from "react-native-paper";
 import { HeaderText } from "./HeaderText";
+import { CategoryTag } from "./CategoryTag";
 import { StarRating } from "./StarRating";
 
 export const SearchItem = ({ item }) => {
