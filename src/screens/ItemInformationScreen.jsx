4d1cc2212f93ae847b07c3a79186950800b47072
Chen858858 <junchenwang5@gmail.com>
2025-01-19 18:17:35 -0800

    ItemInformationScreen added rating stars below title in ItemInformationScreen: imported StarRating (+) added StarRating with item rating & textSize 20

--- src/screens/ItemInformationScreen.jsx
+++ src/screens/ItemInformationScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { HeaderText } from "../components/HeaderText";
+import { StarRating } from "../components/StarRating";
 
 export const ItemInformationScreen = ({ route, items }) => {
   const { itemId } = route.params;
@@ -11,6 +12,7 @@ export const ItemInformationScreen = ({ route, items }) => {
   return (
     <ScrollView style={styles.itemInformationContainer}>
       <HeaderText size="3">{itemInformation["title"]}</HeaderText>
+      <StarRating rating={itemInformation["rating"]} textSize="20" />
     </ScrollView>
   );
 };
